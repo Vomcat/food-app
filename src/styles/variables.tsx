c0e55e17d71820af3e0237a3db912723669acb21
Vomcat <koczfix@gmail.com>
2022-12-11 23:36:26 +0100

    Global styles reset & add fonts

---
+++ src/styles/variables.tsx
@@ -0,0 +1,10 @@
+export const colors = {
+  primary: "#F38181",
+  primaryLight: "rgba(243, 129, 129, 0.29)",
+  secondary: "#95E1D3",
+  dark: "#494747",
+  darkGrey: "#999999",
+  grey: "#D2CDCD",
+  lightGrey: "#D2CDCD",
+  red: "#E01515",
+};
