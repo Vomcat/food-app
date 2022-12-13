e401367fb1e5c1bccfe29c56d737730858cb3cd1
Vomcat <koczfix@gmail.com>
2022-12-13 13:33:02 +0100

    App file clear

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import { GlobalStyles } from "./styles/GlobalStyles";
-import styled from "styled-components";
 
-import { respondFrom } from "./styles/mixins";
-import { breakpoints } from "./styles";
-
-const StyledDiv = styled.div`
-  background-color: black;
-  height: 150px;
-  width: 150px;
-
-  ${respondFrom(breakpoints.mobile)`background-color: red;`}
-`;
+import PageContentContainer from "./components/ui/PageContentContainer";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledDiv></StyledDiv>
     </>
   );
 }
