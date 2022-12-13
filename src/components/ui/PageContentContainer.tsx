a287f1f82bda47fab1644196763778f848861428
Vomcat <koczfix@gmail.com>
2022-12-13 13:37:37 +0100

    Page content container

---
+++ src/components/ui/PageContentContainer.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+import { respondFrom, breakpoints } from "../../styles";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Wrapper = styled.div`
+  padding: 0 16px;
+
+  ${respondFrom(breakpoints.desktop)`
+      max-width: 1232px;
+      margin-inline: auto;
+    `}
+`;
+
+const PageContentContainer: React.FC<Props> = (props) => {
+  return <Wrapper>{props.children}</Wrapper>;
+};
+
+export default PageContentContainer;
