b8be4c9ad89706b4874115b267fd43993000b8ae
Vomcat <koczfix@gmail.com>
2023-01-04 22:34:03 +0100

    Change plus button styles and props

--- src/components/layout/ButtonPlus.tsx
+++ src/components/layout/ButtonPlus.tsx
@@ -4,18 +4,31 @@ import { ButtonStyles } from "interfaces/Layout";
 
 import { colors } from "styles";
 
-const Button = styled.button`
-    border: none;
-    width: 28px;
-    height: 28px;
-    background-color: ${props => props.color ? colors.lightGrey : colors.secondary};;
-    border-radius: 5px;
-`
+import PlusIconWhite from "assets/images/plus.png";
 
-const ButtonPlus: React.FC<ButtonStyles> = ({plus, color, shape})=>{
+const Button = styled.button<ButtonStyles>`
+  position: relative;
+  border: none;
+  cursor: pointer;
+  width: 28px;
+  height: 28px;
+  background-color: ${(props) =>
+    props.variant != "default" ? colors.lightGrey : colors.secondary};
+  border-radius: ${(props) => (props.variant != "default" ? "50%" : "5px")};
+  transition: 0.2s;
 
-    return <Button plus ></Button>
+  &:hover {
+    background-color: ${(props) =>
+      props.variant != "default" ? colors.dark : colors.primary};
+  }
+`;
 
-}
+const ButtonPlus: React.FC<ButtonStyles> = ({ variant = "default" }) => {
+  return (
+    <Button variant={variant} type="button">
+      <img src={PlusIconWhite} alt="plus icon" />
+    </Button>
+  );
+};
 
 export default ButtonPlus;
