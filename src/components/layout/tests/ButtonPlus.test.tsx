a202201a48bf6867d0fc86f84a18797e84e96b76
Vomcat <koczfix@gmail.com>
2023-01-08 23:15:53 +0100

    Button plus tests

---
+++ src/components/layout/tests/ButtonPlus.test.tsx
@@ -0,0 +1,35 @@
+import { render } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import "jest-styled-components";
+
+import ButtonPlus from "../ButtonPlus";
+
+describe("button default image", () => {
+  test("Button render", () => {
+    render(<ButtonPlus />);
+    const testImage = document.querySelector("img") as HTMLImageElement;
+    expect(testImage.alt).toContain("plus icon");
+  });
+});
+
+describe("button minus image", () => {
+  test("Button render", () => {
+    render(<ButtonPlus plusIcon={false} />);
+    const testImage = document.querySelector("img") as HTMLImageElement;
+    expect(testImage.alt).toContain("minus icon");
+  });
+});
+
+describe("button default bg-color", () => {
+  test("Button render", () => {
+    const tree = renderer.create(<ButtonPlus />).toJSON();
+    expect(tree).toHaveStyleRule("background-color", "#95E1D3");
+  });
+});
+
+describe("button minus bg-color", () => {
+  test("Button render", () => {
+    const tree = renderer.create(<ButtonPlus variant="grey" />).toJSON();
+    expect(tree).toHaveStyleRule("background-color", "#D2CDCD");
+  });
+});
--- src/interfaces/Layout.ts
+++ src/interfaces/Layout.ts
@@ -1,3 +1,4 @@
 export interface ButtonStyles {
   variant?: "default" | "grey";
+  plusIcon?: boolean;
 }
