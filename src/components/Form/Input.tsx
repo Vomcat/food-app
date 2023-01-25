e5c220391b28e51ebe98dec43694bedca3a498d2
Vomcat <koczfix@gmail.com>
2023-01-25 14:00:14 +0100

    Add Input component

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,5 +1,27 @@
-const FormInput = () => {
-  return;
-};
+import { forwardRef, InputHTMLAttributes } from "react";
 
-export default FormInput;
+import styled from "styled-components";
+
+import { colors, dimensions, respondFrom, breakpoints } from "styles";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  sizeEl?: "small" | "big";
+}
+
+const InputMain = styled.input`
+  outline: none;
+  border-radius: 16px;
+  border: 1px solid ${colors.secondary};
+  padding: ${dimensions.spacing.sm}px ${dimensions.spacing.md}px;
+
+  ${respondFrom(breakpoints.tablet)`
+    padding: ${dimensions.spacing.sm}px ${dimensions.spacing.md}px;
+  `}
+`;
+
+const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  const { ...inpputAtributes } = props;
+  return <InputMain ref={ref} {...inpputAtributes} />;
+});
+
+export default Input;
