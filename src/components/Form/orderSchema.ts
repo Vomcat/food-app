eb3f9f60d3c5b9dd7c113c40d9e5d4de7d42f67b
Vomcat <koczfix@gmail.com>
2023-01-26 12:05:08 +0100

    Add new validation parameters

--- src/components/Form/orderSchema.ts
+++ src/components/Form/orderSchema.ts
@@ -1,5 +1,8 @@
 import * as yup from "yup";
 
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
 export const orderSchema = yup.object().shape({
   name: yup
     .string()
@@ -11,10 +14,13 @@ export const orderSchema = yup.object().shape({
     .required("Last Name is required")
     .min(2, "Last Name must be at least 2 characters")
     .max(30, "Last Name must not exceed 30 characters"),
-  email: yup.string().required("Fullname is required"),
-  phoneNumber: yup.string().required("Fullname is required"),
-  streetAddress: yup.string().required("Fullname is required"),
-  streetNumber: yup.string().required("Fullname is required"),
-  city: yup.string().required("Fullname is required"),
-  postCode: yup.string().required("Fullname is required"),
+  email: yup.string().email().required("Email is required"),
+  phoneNumber: yup
+    .string()
+    .matches(phoneRegExp, "Phone number is not valid")
+    .required("Phone number is required"),
+  streetAddress: yup.string().required("Street Addres is required"),
+  streetNumber: yup.string().required("Street number is required"),
+  city: yup.string().required("City is required"),
+  postCode: yup.string().required("Post code is required"),
 });
