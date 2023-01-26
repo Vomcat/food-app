ca2f26045dd8be3f2dfc964e628ddf7e5e1cb162
Vomcat <koczfix@gmail.com>
2023-01-26 12:05:38 +0100

    Add new variable for inputstyle in Form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -11,9 +11,11 @@ import { orderSchema } from "components/Form/orderSchema";
 import Input from "./Input";
 import Button from "components/Ui/Button";
 
-const FormWrapper = styled.div`
-  flex: 1;
-`;
+interface FormStylesProps {
+  inputSize?: string;
+}
+
+const FormWrapper = styled.div``;
 
 const FormHeading = styled.h1`
   font-size: ${dimensions.fonts.heading}px;
@@ -34,7 +36,7 @@ const InputsWrapper = styled.div`
 `}
 `;
 
-const InputGroup = styled.div`
+const InputGroup = styled.div<FormStylesProps>`
   position: relative;
   padding-bottom: ${dimensions.spacing.md}px;
   display: flex;
@@ -42,15 +44,20 @@ const InputGroup = styled.div`
 
   ${respondFrom(breakpoints.tablet)`
     padding-bottom: ${dimensions.spacing.md3}px;
-    ${(props: { sizeEl: string }) =>
-      props.sizeEl === "small" ? `flex: 50%;` : "flex:70%"}
+    flex: ${(props: { inputSize: string }) =>
+      props.inputSize === "small"
+        ? `15%;`
+        : props.inputSize === "big"
+        ? "85%"
+        : `50%`};
   `}
 `;
 
 const InputErrorMessage = styled.p`
   position: absolute;
-  left: 5px;
+  left: ${dimensions.spacing.md}px;
   bottom: ${dimensions.spacing.md}px;
+  font-family: ${fonts.primaryItalic};
   font-size: ${dimensions.fonts.xsmall}px;
   color: ${colors.red};
 `;
@@ -88,6 +95,7 @@ const Form = () => {
         <InputsWrapper>
           <InputGroup>
             <Input placeholder="Email" type="email" {...register("email")} />
+            <InputErrorMessage>{errors.email?.message}</InputErrorMessage>
           </InputGroup>
           <InputGroup>
             <Input
@@ -95,34 +103,43 @@ const Form = () => {
               type="text"
               {...register("phoneNumber")}
             />
+            <InputErrorMessage>{errors.phoneNumber?.message}</InputErrorMessage>
           </InputGroup>
         </InputsWrapper>
         <InputsWrapper>
-          <InputGroup>
+          <InputGroup inputSize="big">
             <Input
               placeholder="Street address"
               type="text"
               {...register("streetAddress")}
             />
+            <InputErrorMessage>
+              {errors.streetAddress?.message}
+            </InputErrorMessage>
           </InputGroup>
-          <InputGroup>
+          <InputGroup inputSize="small">
             <Input
               placeholder="Street number"
               type="text"
               {...register("streetNumber")}
             />
+            <InputErrorMessage>
+              {errors.streetNumber?.message}
+            </InputErrorMessage>
           </InputGroup>
         </InputsWrapper>
         <InputsWrapper>
-          <InputGroup>
+          <InputGroup inputSize="big">
             <Input placeholder="City" type="text" {...register("city")} />
+            <InputErrorMessage>{errors.city?.message}</InputErrorMessage>
           </InputGroup>
-          <InputGroup>
+          <InputGroup inputSize="small">
             <Input
               placeholder="Post code"
               type="text"
               {...register("postCode")}
             />
+            <InputErrorMessage>{errors.postCode?.message}</InputErrorMessage>
           </InputGroup>
         </InputsWrapper>
         <Button type="submit">Order</Button>
