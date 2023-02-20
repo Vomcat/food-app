6256240495c3479e08c52c9d6271b4771e2fad3d
koczfi <koczfix@gmail.com>
2023-02-20 13:25:35 +0100

    Banner tests

---
+++ src/components/Banner/__tests__/Banner.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "utils/utils-for-tests";
+import '@testing-library/jest-dom'
+
+
+import Banner from "components/Banner/Banner";
+
+describe("Navigation component", () => {
+  test("Text is correct", () => {
+    render(<Banner />);
+  
+
+    expect(screen.getByText(/Welcome to/i)).toBeInTheDocument();
+    expect(screen.getByRole('heading', {
+        name: /papu store/i
+      })).toBeInTheDocument();
+  });
+});
