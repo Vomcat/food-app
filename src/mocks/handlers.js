5400feda80bca82d2d87e556f3bc8283d1341a75
koczfi <koczfix@gmail.com>
2023-02-21 20:53:27 +0100

    Add mock for orders

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,8 +1,12 @@
 import { rest } from "msw";
-import { MOCK_DATA } from "mocks/mockedData";
+import { MOCK_DATA_PRODUCTS, MOCK_DATA_ORDERS } from "mocks/mockedData";
 
 export const handlers = [
   rest.get(`${process.env.REACT_APP_API_HOST}/items`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(MOCK_DATA));
+    return res(ctx.status(200), ctx.json(MOCK_DATA_PRODUCTS));
+  }),
+
+  rest.get(`${process.env.REACT_APP_API_HOST}/order`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(MOCK_DATA_ORDERS));
   }),
 ];
--- src/mocks/mockedData.ts
+++ src/mocks/mockedData.ts
@@ -1,4 +1,4 @@
-export const MOCK_DATA = [
+export const MOCK_DATA_PRODUCTS = [
   {
     id: 1,
     image: "/assets/images/item1.png",
@@ -12,3 +12,33 @@ export const MOCK_DATA = [
     price: 5,
   },
 ];
+
+export const MOCK_DATA_ORDERS = [{
+  orderItems: [
+    {
+      id: 1,
+      name: "Mniam Mniam Food",
+      price: 5,
+      quantity: 4,
+      totalPrice: 20
+    },
+    {
+      id: 3,
+      name:" Spicy noodle with shrimps",
+      price: 7,
+      quantity: 2,
+      totalPrice: 14
+    },
+  ],
+  name: "name",
+  orderTotalPrice: 13,
+  lastName: "last name",
+  email: "mail@wp.pl",
+  phoneNumber: 123123,
+  streetAddress: "address",
+  streetNumber: "13",
+  postCode: 123,
+  city: "Sopot",
+  id: 1
+},
+]
