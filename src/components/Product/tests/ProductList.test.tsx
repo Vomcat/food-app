b9899622c18640c28852d1755e39c55d1f412101
koczfi <koczfix@gmail.com>
2023-02-21 20:55:23 +0100

    Update product list tests

--- src/components/Product/tests/ProductList.test.tsx
+++ src/components/Product/tests/ProductList.test.tsx
@@ -2,12 +2,19 @@ import { render, screen } from "utils/utils-for-tests";
 
 import ProductList from "components/Product/ProductList";
 
+import { MOCK_DATA_PRODUCTS } from "mocks/mockedData";
+
 describe("Product List render test", () => {
-  it("renders correctly", async () => {
+  test("Products list renders correctly", async () => {
     const { asFragment } = render(<ProductList />);
 
-    await screen.findByText("Mniam Mniam Food");
+    await screen.findByText(MOCK_DATA_PRODUCTS[0].name);
 
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  test("Products list renders correctly", async () => {
+     render(<ProductList />);
+
   });
 });
